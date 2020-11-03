e340de35d28f49a4685ee5f0f381b1280874b9d8
manyathakur <70948896+manyathakur@users.noreply.github.com>
2020-11-03 19:50:41 +0530

    Add files via upload

---
+++ CSGO/sketch.js
@@ -0,0 +1,29 @@
+var car,wall;
+var speed,weight;
+function setup() {
+  createCanvas(1600,400);
+  car=createSprite(50,200,50,25)
+  wall=createSprite(1500,200,25,height/2)
+  speed=random(55,95)
+  weight=random(400,1500)
+}
+
+function draw() {
+  background(0,0,0);  
+  car.velocityX=speed;
+  wall.shapeColor=color(169,169,169)
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.velocityX=0;
+    var deformation=0.5 *weight * speed* speed/22509
+    if(deformation>180){
+      car.shapeColor=color(255,0,0);
+    }
+    if(deformation<180 && deformation>100){
+      car.shapeColor=color(255,255,0)
+    }
+    if(deformation<100){
+      car.shapeColor=color(0,255,0)
+    }
+  }
+  drawSprites();
+}
